86bb0f6b6d8c178a1a5b736a5f86313fb839528c
Roman Leontev <974078rl@gmail.com>
2020-07-18 02:29:57 +0300

    dz get info

---
+++ src/ru/kpfu/itis/nodejs/app/routes/users_routes.js
@@ -0,0 +1,16 @@
+module.exports = function (app) {
+    app.get('/users', (request, response) => {
+        var result = [
+            {
+                "name": "Роман",
+                "surname": "Леонтьев",
+                "address": "Казань",
+                "phoneNumber": 89196411599,
+                "best-skill": "Умею драться клюшкой на коньках",
+                "information": "Всем привет! Меня зовут Роман Леонтьев мне 18 лет. " +
+                    "Я начинающий java-разработчик, закончил первый курс в ВШ ИТИС"
+            }
+        ];
+        response.send(JSON.stringify(result));
+    });
+};
--- src/ru/kpfu/itis/nodejs/server.js
+++ src/ru/kpfu/itis/nodejs/server.js
@@ -1,9 +1,13 @@
 // используем библиотеку express
 const express = require('express');
+const bodyParser = require('body-parser');
 // создаем объект express
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+require('./app/routes')(app);
 // говорим, что мы раздаем папку public
-app.use(express.static('public'));
+app.use(express.static('public/siteSummary'));
 // говорим, что запускаемся на порту 80
 app.listen(80);
 console.log("Server started at 80");
+
