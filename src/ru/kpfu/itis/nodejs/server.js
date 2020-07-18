7c0c651e3cf9291d50db89af89b255d8938a10be
Roman Leontev <974078rl@gmail.com>
2020-07-18 10:35:52 +0300

    dz "get information" last modification

--- src/ru/kpfu/itis/nodejs/server.js
+++ src/ru/kpfu/itis/nodejs/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./app/routes')(app);
 // говорим, что мы раздаем папку public
-app.use(express.static('public/siteSummary'));
+app.use(express.static('public'));
 // говорим, что запускаемся на порту 80
 app.listen(80);
 console.log("Server started at 80");
