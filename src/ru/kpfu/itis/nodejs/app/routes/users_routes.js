d8a3ee5d156bcbe455834777a4199263b1d8328b
Roman Leontev <974078rl@gmail.com>
2020-07-18 13:04:47 +0300

    dz "get information" last modification

--- src/ru/kpfu/itis/nodejs/app/routes/users_routes.js
+++ src/ru/kpfu/itis/nodejs/app/routes/users_routes.js
@@ -7,6 +7,8 @@ module.exports = function (app) {
                 "address": "Казань",
                 "phoneNumber": 89196411599,
                 "best-skill": "Умею драться клюшкой на коньках",
+                "e-mail": "974078rl@gmail.com",
+                "telegram": "@romych13",
                 "information": "Всем привет! Меня зовут Роман Леонтьев мне 18 лет. " +
                     "Я начинающий java-разработчик, закончил первый курс в ВШ ИТИС"
             }
